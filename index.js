14bca35d0e34d9c81ef1eaa1d88beb5efd147fb4
Talia <lia12303@gmail.com>
2023-03-18 21:25:06 -0400

    Fun Javascript Calculator

---
+++ index.js
@@ -0,0 +1,13 @@
+
+if (operator == '+') { // use + (addition) operator to add two numbers
+    result = number1 + number2;
+}
+else if (operator == '-') { // use -  (subtraction) operator to subtract two numbers
+    result = number1 - number2;
+}
+else if (operator == '*') { // use * (multiplication) operator to multiply two numbers
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2; // use / (division) operator to divide two numbers
+}
